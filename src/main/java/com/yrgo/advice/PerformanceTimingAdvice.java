5e9086ae4bc2f95e57f0ca391719a2be8f29591f
Poya Kazemzade <pouya.pua@gmail.com>
2024-11-19 13:01:15 +0100

    debuged assignment 4

--- src/main/java/com/yrgo/advice/PerformanceTimingAdvice.java
+++ src/main/java/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -1,2 +1,29 @@
-package com.yrgo.advice;public class PerformanceTimingAdvice {
+package com.yrgo.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class PerformanceTimingAdvice {
+
+    //before
+    public Object performTimingMeasurement(ProceedingJoinPoint method)
+            throws Throwable {
+
+        long startTime = System.nanoTime();
+
+        try {
+            //proceed to target
+            Object value = method.proceed();
+            return value;
+        } finally {
+            //after
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+            String methodName = method.getSignature().getName();
+            String className = method.getTarget().getClass().getName();
+            System.out.println("Time taken for the method " +
+                    methodName + "from the class " + className + " took " + timeTaken
+                    / 1000000 + "ms");
+        }
+    }
 }
